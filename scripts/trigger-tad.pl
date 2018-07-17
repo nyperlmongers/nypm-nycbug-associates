b61005f5daa69962c31ed60f46ec789c2a0e0b8e
James E Keenan <jkeenan@cpan.org>
2018-07-17 09:50:12 -0400

    Adapted for use on FreeBSD (perl-reporter-03).
    Before debugging, used to run perl-5.29.0.  The run was not successful
    as run_cpanm() did not complete, hence no analysis/*, etc., files were
    created and no consolidation done.
    
    However, after debugging, I was able to run the program all the way
    through with a small set of CPAN modules.

--- scripts/trigger-tad.pl
+++ scripts/trigger-tad.pl
@@ -8,10 +8,13 @@ use Perl::Download::FTP;
 use Email::Sender::Simple qw(sendmail);
 use Email::Simple;
 use Email::Simple::Creator;
+use File::Copy;
 use File::Spec;
 use File::Temp qw(tempdir);
 use Getopt::Long;
 use Test::Against::Dev;
+use Test::Against::Dev::Sort;
+use Test::Against::Dev::ProcessPSV;
 
 =head1 NAME
 
@@ -19,7 +22,8 @@ trigger-tad.pl - Test top of CPAN river against monthly Perl development release
 
 =head1 OBJECTIVE
 
-This program is intended to be run once a month during an annual Perl development cycle.  Each time it runs:
+This program is intended to be run once a month during an annual Perl
+development cycle.  Each time it runs:
 
 =over 4
 
@@ -59,16 +63,21 @@ In real production, that will kick off the test-against-dev process.
     --hostdir=/pub/languages/perl/CPAN/src/5.0 \
     --minor_version=29 \
     --compression=gz \
-    --river_file=/home/jkeenan/var/tad/src/modules.XXXXXXXX.cpan.river.txt \
+    --river_file=/home/jkeenan/var/tad/src/modules-for-cpanm-20180626.txt \
     --title=cpan-river-3000 \
     --email_to='"James E Keenan" <jkeenan@pobox.com>' \
-    --email_from='"Jim Keenan Linode" <jkeenan@thenceforward.net>' \
+    --email_from='"James E Keenan" <jkeenan@pobox.com>' \
     --email_subject='Status of Perl 5 development release' \
     --verbose
 
     --testing
 
 =cut
+
+my $date = qx/date/;
+chomp $date;
+say sprintf("%-52s%s" => ("Date:", $date));
+say "Running $0";
 
 my ($application_dir, $tdir, $compression, $host, $hostdir,
     $river_file, $minor_version, $title,
@@ -247,7 +256,15 @@ if ($testing) {
     exit 0;
 }
 
-my $this_perl = $tadobj->configure_build_install_perl({ verbose => $verbose });
+my $freebsd_configure_command =
+    "sh ./Configure -des -Dusedevel  -Uversiononly -Dman1dir=none -Dman3dir=none";
+$freebsd_configure_command .= qq| -Duseithreads -Doptimize="-O2 -pipe -fstack-protector -fno-strict-aliasing"|;
+$freebsd_configure_command .= " -Dprefix=$release_dir";
+
+my $this_perl = $tadobj->configure_build_install_perl({
+    configure_command => $freebsd_configure_command,
+    verbose => $verbose,
+});
 unless (-f $this_perl) {
     croak "Failed to install perl";
 }
@@ -331,6 +348,14 @@ else {
 
 chdir $cwd;
 
+my $master_psvfile = consolidate_psvfiles( {
+    results_dir     => $tadobj->get_results_dir(),
+    perldevcycle    => "perl-5.$minor_version",
+    title           => $title,
+    verbose         => $verbose,
+} );
+say "See master psvfile in: $master_psvfile";
+
 #$email_body = "Completed run of $0 for $latest_on_server";
 #email_notify( {
 #    email_to            => $email_to,
@@ -340,7 +365,7 @@ chdir $cwd;
 #    email_body          => $email_body,
 #} );
 
-say "\nFinished!";
+say "\nFinished!\n";
 exit 0;
 
 ##### SUBROUTINES #####
@@ -405,4 +430,127 @@ sub email_notify {
     else { return 1 };
 }
 
+sub consolidate_psvfiles {
+    my $args = shift;
+    croak "Argument to consolidate_psvfiles() must be hashref"
+        unless ref($args) eq 'HASH';
+    my %required_args = map { $_ => 1 } ( qw| results_dir perldevcycle title | );
+    for my $key (keys %required_args) {
+        croak "Hashref element '$key' missing" unless exists $args->{$key};
+    }
+    my $verbose = $args->{verbose} || '';
+
+    my ($dev_minor) = $args->{perldevcycle} =~ m/5\.(\d{1,2})$/;
+    my $rc_minor = $dev_minor + 1;
+    our $dev_pattern = qr/^perl-5\.($dev_minor)\.(\d{1,2})/;
+    our $rc_pattern  = qr/^perl-5\.($rc_minor)\.(\d{1,2})-RC(\d)/;
+
+    # Detect the Perl dev or RC releases which have been processed already
+    # by test-against-dev
+
+    my %versions = ();
+    opendir my $DIRH, $args->{results_dir} or croak "Unable to open $args->{results_dir} for reading";
+    my @seen = grep { ! m/^\.{1,2}$/ } readdir $DIRH;
+    closedir $DIRH or croak "Unable to close $args->{results_dir} after reading";
+    #dd(\@seen);
+
+    for my $e (@seen) {
+        if ($e =~ m/($dev_pattern|$rc_pattern)$/) {
+            my $perl_version = $1;
+            my $version_dir = File::Spec->catdir($args->{results_dir}, $perl_version);
+            croak "Could not locate directory '$version_dir'" unless -d $version_dir;
+            my $version_storage_dir = File::Spec->catdir($version_dir, 'storage');
+            croak "Could not locate directory '$version_storage_dir'" unless -d $version_storage_dir;
+            my $psvfile = File::Spec->catfile($version_storage_dir,
+                "$args->{title}.${perl_version}.psv"
+            );
+            croak "Could not locate file '$psvfile'" unless -f $psvfile;
+            say join('|' => $perl_version, $version_dir, $version_storage_dir, $psvfile);
+            $versions{$perl_version}{version_dir} = $version_dir;
+            $versions{$perl_version}{version_storage_dir} = $version_storage_dir;
+            $versions{$perl_version}{psvfile} = $psvfile;
+        }
+    }
+    if ($verbose) {
+        say "Observed this structure in \%versions:";
+        dd(\%versions);
+    }
+
+    # See if we already have a master PSV file
+
+    my $master_file = File::Spec->catfile(
+        $args->{results_dir},
+        "$args->{title}.$args->{perldevcycle}.master.psv"
+    );
+    say "master_file will be $master_file" if $verbose;
+    if (! -f $master_file) {
+        say "$master_file does not yet exist" if $verbose;
+        my $key = "$args->{perldevcycle}.0";
+        if (
+            (scalar keys %versions == 1) and
+            (exists $versions{$key})
+        ) {
+            say "Copying $versions{$key}{psvfile} to $master_file"
+                if $verbose;
+            copy $versions{$key}{psvfile} => $master_file
+                or croak "Unable to copy $versions{$key}{psvfile} to $master_file";
+            say "\nFinished!";
+            exit 0;
+        }
+        else {
+            croak "Don't know how to handles this situation";
+        }
+    } # This block either exit 0 or croak.
+
+    say "$master_file exists" if $verbose;
+    # As precaution, backup the master file and work from it.
+    my $backup_master = "$master_file.bak";
+    copy $master_file => $backup_master
+        or croak "Unable to copy $master_file to $backup_master";
+
+    my $ppsv = Test::Against::Dev::ProcessPSV->new( { verbose => 1 } );
+    my $columns_seen = $ppsv->read_one_psv( { psvfile => $backup_master } );
+    croak "read_one_psv() did not return populated array ref"
+        unless ( (ref($columns_seen) eq 'ARRAY') and (scalar(@{$columns_seen}) >= 5) );
+    my ($highest_version_in_master) = $columns_seen->[-1] =~ m/^(.*?)\.grade$/;
+    croak "Unable to get highest version in master psv file"
+        unless (
+            $highest_version_in_master and
+            ($highest_version_in_master =~ m/^($dev_pattern|$rc_pattern)$/ )
+        );
+    say "Highest perl version found in master psv: $highest_version_in_master" if $verbose;
+
+    my $tads_object = Test::Against::Dev::Sort->new($dev_minor);
+    my $sorted_versions_ref = $tads_object->sort_dev_and_rc_versions([ keys %versions ]);
+    dd($sorted_versions_ref);
+    my $most_recent_version = $sorted_versions_ref->[-1];
+    say "Most recent Perl version processed:       ", $most_recent_version if $verbose;
+    my $most_recent_psvfile = $versions{$most_recent_version}{psvfile};
+    say "Most recent psvfile:                      ", $most_recent_psvfile if $verbose;
+
+    my $columns_seen2 = $ppsv->read_one_psv( { psvfile => $most_recent_psvfile } );
+    croak "read_one_psv() did not return populated array ref"
+        unless ( (ref($columns_seen2) eq 'ARRAY') and (scalar(@{$columns_seen2}) >= 5) );
+
+
+    say scalar keys %{$ppsv->{master_data}}, " elements in master_data" if $verbose;
+
+    my $master_columns_ref = ['dist'];
+    my @fields = ( qw| author distname distversion grade | );
+    for my $pv (@{$sorted_versions_ref}) {
+        for my $f (@fields) {
+            push @{$master_columns_ref}, "$pv.$f";
+        }
+    }
+
+    my $rv = $ppsv->write_master_psv( {
+        master_columns  => $master_columns_ref,
+        master_psvfile  => $master_file,
+    } );
+
+    croak "$master_file not created" unless (-f $master_file);
+    say "Created $master_file" if $verbose;
+
+    return $master_file;
+}
 __END__
