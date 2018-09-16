99806ee43b03d7639974bf0504ead67e13d051ab
James E Keenan <jkeenan@cpan.org>
2018-09-16 10:12:09 -0400

    Begin NYCBUG presentation.

---
+++ presentations/nycbug-201811/metadata.pl
@@ -0,0 +1,12 @@
+# source for /home/jkeenan/bin/perl/prepare_presentation.pl
+%metadata = (
+    stylesheet      => "jkeenan_traditional.css",
+    date            => "November 07 2018",
+    bar_title       => "Ensuring Perl's Viability on FreeBSD: A NYCBUG-NY.PM Collaboration",
+    copyright       => "2018",
+    text_title      => "Ensuring Perl's Viability on FreeBSD: A NYCBUG-NY.PM Collaboration",
+    venue           => "New York City BSD User Group/New York Perlmongers",
+    class           => "viability",
+);
+
+1;
